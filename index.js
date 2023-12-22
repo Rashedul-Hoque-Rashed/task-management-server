0a7edda3071bc613a04b9059579ed63c173145b3
Rashed <rashedulhoqueceo@gmail.com>
2023-12-22 19:33:45 +0600

    add netlify orizin

--- index.js
+++ index.js
@@ -7,13 +7,9 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({
-    origin: ['http://localhost:5173',
-    'https://task-management-dfa59.web.app',
-    'https://task-management-dfa59.firebaseapp.com',
-],
-    credentials: true,
-}));
+
+
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
@@ -32,19 +28,6 @@ const client = new MongoClient(uri, {
 });
 
 
-const verifyToken = (req, res, next) => {
-    const token = req?.cookies?.token;
-    if (!token) {
-        return res.status(401).send({ message: 'unauthorized access' })
-    }
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(401).send({ message: 'unauthorized access' })
-        }
-        req.user = decoded;
-        next();
-    })
-}
 
 
 async function run() {
@@ -59,7 +42,7 @@ async function run() {
             res.send(result)
         })
 
-        app.get('/tasks/:email', verifyToken, async (req, res) => {
+        app.get('/tasks/:email', async (req, res) => {
             const email = req.params.email;
             const query = { userEmail: email }
             const result = await taskCollections.find(query).toArray();
@@ -80,7 +63,7 @@ async function run() {
             res.send(result)
         })
 
-        app.put("/update/:id", verifyToken, async (req, res) => {
+        app.put("/update/:id", async (req, res) => {
             const id = req.params.id;
             const task = req.body;
             const filter = {_id: new ObjectId(id)};
@@ -97,7 +80,7 @@ async function run() {
             res.send(result);
           })
 
-        app.put("/tasks/status/:id", verifyToken, async (req, res) => {
+        app.put("/tasks/status/:id", async (req, res) => {
             const id = req.params.id;
             const task = req.body;
             const filter = {_id: new ObjectId(id)};
@@ -111,29 +94,12 @@ async function run() {
             res.send(result);
           })
 
-        app.post('/tasks', verifyToken, async (req, res) => {
+        app.post('/tasks', async (req, res) => {
             const task = req.body;
             const result = await taskCollections.insertOne(task);
             res.send(result)
         })
 
-        app.post('/jwt', async (req, res) => {
-            const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2h' });
-
-            res.cookie('token', token, {
-                httpOnly: true,
-                secure: true,
-                sameSite: 'none'
-            })
-                .send({ success: true });
-        })
-
-        app.post('/logout', async (req, res) => {
-            const user = req.body;
-            console.log('logging out', user);
-            res.clearCookie('token', { maxAge: 0 }).send({ success: true })
-        })
 
 
         // Send a ping to confirm a successful connection
